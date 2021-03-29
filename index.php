d9c1a07d424711d4cc039ef2cd275a9520d647fd
GTPE Student <simona.posca-accenture.com@bastion.689d.internal>
2021-03-29 11:32:26 +0000

    index updated

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html>
 
 <head>
-<title>Hello World version 2!</title>
+<title>Hello World!</title>
 </head>
 
 <body>
 
 <?php
-  echo "Hello World in PHP - v2!";
+  echo "Hello World in PHP!";
 ?>
 
 </body>
